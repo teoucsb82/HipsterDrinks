c8e973bb61999a6269da17a81c180469f297a1d4
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-03-19 09:49:27 -0700

    Tweaked emails

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,5 +1,5 @@
 class TestMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Hipster Drinks <admin@hipsterdrinks.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
