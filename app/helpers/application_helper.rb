3760f6db6d1d84b1259b4f770cffd7cc88a48218
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-03-19 13:58:08 -0700

    Added title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-
+	def full_title(page_title)
+    base_title = "Hipster Drinks"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
