774c55cdf6d6238869fd3a0830b5fae136f3ffd4
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-03-18 22:54:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HipsterDrinks::Application.config.secret_key_base = 'f9f7bf29cf09f4fff5eb2a35daceeda4986a7a65ac3fea4cdc6b9860ed6550b622e9e20faed3b33923cdc56567d49c30bd08f0e306783173685b7f3da59b102e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HipsterDrinks::Application.config.session_store :cookie_store, key: '_hipster_drinks_session'
