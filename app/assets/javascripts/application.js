c9c28569414f47816b8e1237b2b551cdd826a845
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-03-21 12:39:35 -0700

    Removed backbone, starting over with Drink CRUD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,3 @@
 //= require jquery_ujs
 //= require jquery.serializeJSON
 //= require bootstrap
-//= require underscore
-//= require backbone
-//
-//= require hipster_drinks
-//
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
--- app/assets/javascripts/collections/drinks.js
+++
@@ -1,4 +0,0 @@
-HipsterDrinks.Collections.Drinks = Backbone.Collection.extend({
-	model: HipsterDrinks.Models.Drink,
-	url: "/drinks"
-});
